b8d27a0d2a86439861b7c781aaf6fde62a4f007f
sebastian.dev <sebastian.villalob@gmail.com>
2022-05-04 18:40:40 -0300

    Terminando el Header

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
-import HeaderOption from './HeaderOption';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter'
+import ChatIcon from '@mui/icons-material/Chat'
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HeaderOption from './HeaderOption';
 
 const Header = () => {
   return (
@@ -18,10 +21,13 @@ const Header = () => {
       </div>
 
       <div className='header__right'>
-        <HeaderOption Icon={HomeIcon} title='Home' />
-        <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+        <HeaderOption Icon={HomeIcon} title='Inicio' />
+        <HeaderOption Icon={SupervisorAccountIcon} title='Mi Red' />
+        <HeaderOption Icon={BusinessCenterIcon} title='Empleos' />
+        <HeaderOption Icon={ChatIcon} title='Mensajes' />
+        <HeaderOption Icon={NotificationsIcon} title='Notificaciones' />
+        <HeaderOption avatar='/images/profile.png' title='Yo' />
       </div>
-
     </div>
   )
 }
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './HeaderOption.css';
+import { Avatar } from '@material-ui/core';
 
-const HeaderOption = ({ Icon, title }) => {
+const HeaderOption = ({ Icon, title, avatar }) => {
   return (
     <div className='header__option'>
       {Icon && <Icon className='header__option-icon' />}
+      {avatar && <Avatar className='header__option-icon' src={avatar} />}
       <h3 className='header__option-title'>{title}</h3>
     </div>
   )
