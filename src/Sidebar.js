23395739a06397959293c4c73d70aa2eddc602f7
sebastian.dev <sebastian.villalob@gmail.com>
2022-05-11 19:13:15 -0300

    Añadiendo sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,6 +11,21 @@ const Sidebar = () => {
         <h2>Sebastián Villalobos</h2>
         <h4>sebastian.villalob@gmail.com</h4>
       </div>
+
+      <div className='sidebar__stats'>
+        <div className='sidebar__stat'>
+          <p>Quien ha visto tu perfil</p>
+          <p className='sidebar__stat-number'>61</p>
+        </div>
+        <div className='sidebar__stat'>
+          <p>Impresiones de tu publicación</p>
+          <p className='sidebar__stat-number'>70</p>
+        </div>
+
+        <div className='sidebar__button'>
+          <p>Recientes</p>
+        </div>
+      </div>
     </div>
   )
 }
