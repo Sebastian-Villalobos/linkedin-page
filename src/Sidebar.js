f92554255cb6c58557195720a0685bc447bada78
sebastian.dev <sebastian.villalob@gmail.com>
2022-05-12 20:21:35 -0300

    Terminando sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,17 @@ import React from 'react';
 import './Sidebar.css';
 
 const Sidebar = () => {
+
+  const recentItem = (topic) => {
+    <div className='sidebar__recentItem'>
+
+    </div>
+  }
+
   return (
     <div className='sidebar'>
       <div className='sidebar__top'>
-        <img src='' alt='' />
+        <img src='/images/background.jpg' alt='' />
         <Avatar className='sidebar__avatar' />
         <h2>Sebastián Villalobos</h2>
         <h4>sebastian.villalob@gmail.com</h4>
@@ -15,11 +22,11 @@ const Sidebar = () => {
       <div className='sidebar__stats'>
         <div className='sidebar__stat'>
           <p>Quien ha visto tu perfil</p>
-          <p className='sidebar__stat-number'>61</p>
+          <p className='sidebar__statNumber'>61</p>
         </div>
         <div className='sidebar__stat'>
           <p>Impresiones de tu publicación</p>
-          <p className='sidebar__stat-number'>70</p>
+          <p className='sidebar__statNumber'>70</p>
         </div>
 
         <div className='sidebar__button'>
